fe3cddf44014a0a4f1873179a3201408212fe79d
PramaYudhistira <pyudhistira02@gmail.com>
2024-11-08 22:39:50 -0600

    welcome card WIP and starting work on Navbar morphing idea

--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -1,12 +1,11 @@
-// src/app/page.tsx
 import Navbar from "../components/Navbar";
+import WelcomeCard from "@/components/WelcomeCard";
 
 export default function Home() {
   return (
     <div className="min-h-screen">
       <div style={{ height: '200vh' }}> 
-      {/* This div adds height to enable scrolling,
-      will add actual content once i figure out how to fix this flickering issue*/}
+        <WelcomeCard />
       </div>
       <Navbar />
     </div>
--- portfolio/src/components/Navbar.tsx
+++ portfolio/src/components/Navbar.tsx
@@ -41,7 +41,10 @@ const Navbar: React.FC = () => {
         }}
         >
         <nav className="flex justify-around">
-            <a href="#home" className="hover:bg-indigo-500 transition
+            <a href="#" onClick={(e) => {
+                e.preventDefault();
+                window.scrollTo({ top: 0, behavior: "smooth" });
+            }} className="hover:bg-indigo-500 transition
             ease-in-out hover:-translate-y-1 hover:translate-y-1 hover:scale-110 
             rounded-full px-4 py-2 border border-transparent">Home</a>
         </nav>
