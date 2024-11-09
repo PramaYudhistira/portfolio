40d4cab7fca7ffcafc223eaf3d7c2bac9a8cce64
PramaYudhistira <pyudhistira02@gmail.com>
2024-11-09 10:20:11 -0600

    it actually works now

--- portfolio/src/components/BackgroundVideo.tsx
+++ portfolio/src/components/BackgroundVideo.tsx
@@ -7,12 +7,19 @@ const BackgroundVideo: React.FC = () => {
         autoPlay
         loop
         muted
+        playsInline
         className="min-w-full min-h-full object-cover"
       >
-        <source src="/public/videos/background.mp4" type="video/mp4" />
+        <source src="/videos/background.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
-      <div className="absolute bottom-0 left-0 w-full h-1/3 bg-gradient-to-t from-black to-transparent"></div>
+      <div className="absolute bottom-0 left-0 w-full 
+      h-1/3 bg-gradient-to-t from-black to-transparent"
+      style={{
+        background:
+          "linear-gradient(to bottom, rgba(0,0,0,0) 70%, rgba(0,0,0,1)), linear-gradient(to right, rgba(0,0,0,1), rgba(0,0,0,0) 20%, rgba(0,0,0,0) 80%, rgba(0,0,0,1))",
+        mixBlendMode: "multiply",
+      }}></div>
     </div>
   );
 };
