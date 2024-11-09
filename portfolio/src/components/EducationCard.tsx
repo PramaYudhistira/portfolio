34e2bc59172fa635b7417d3ffcc402fbf9e768bb
PramaYudhistira <pyudhistira02@gmail.com>
2024-11-09 17:52:31 -0600

    separated education into columns

--- portfolio/src/components/EducationCard.tsx
+++ portfolio/src/components/EducationCard.tsx
@@ -1,19 +1,28 @@
+"use client";
 import React from "react";
 
 const EducationCard: React.FC = () => {
   return (
-    <div className="shadow-md rounded-lg p-6 max-w-md mx-auto"
-    style={{
-        marginTop: "20rem",
-    }}>
 
-        <div className="text-left mb-4 items-left left-0">
+    <div className="flex items-start" style={{
+        marginTop: "25rem"
+    }}>
+        <div className="text-left mb-4 items-left left-0
+        pl-10">
             {/*perhaps add cards of images 
             under this div... */}
             <h1 className="text-5xl">About me</h1>
         </div>
 
-        <p className="text-white text-lg">Details about your education go here.</p>
+        <div className="flex justify-center pl-20 text-left"
+        style={{
+            marginTop: "1rem"
+        }}>
+            <p className="text-white text-lg text-center">Details about your education go here
+                asdfuhasdifuhaidufhvsidjfvksjdfbvkjhbzsdvbalkjdhfgkljdfvnlksjncvlkjnwf.</p>
+
+        </div>
+
     </div>
   );
 };
