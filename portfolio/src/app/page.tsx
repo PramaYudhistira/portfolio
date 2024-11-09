708bf13aa4a33f5708ee7e71fa48e5b832cdc6a7
PramaYudhistira <pyudhistira02@gmail.com>
2024-11-08 20:31:52 -0600

    refactor code styling so its more readable

--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -4,7 +4,6 @@ import Navbar from "../components/Navbar";
 export default function Home() {
   return (
     <div className="min-h-screen">
-      <h1>Test</h1>
       <div style={{ height: '200vh' }}> 
       {/* This div adds height to enable scrolling,
       will add actual content once i figure out how to fix this flickering issue*/}
--- portfolio/src/components/Navbar.tsx
+++ portfolio/src/components/Navbar.tsx
@@ -4,53 +4,49 @@
 import React, { useEffect, useRef, useState } from "react";
 
 const Navbar: React.FC = () => {
-  const navbarRef = useRef<HTMLDivElement>(null);
-  const [isSticky, setIsSticky] = useState(false);
-  //start of 
-  useEffect(() => {
-    //handleScroll function
-    const handleScroll = () => {
-      if (navbarRef.current) {
-        const navbarTop = navbarRef.current.getBoundingClientRect().top;
+    const [isSticky, setIsSticky] = useState(false);
+    const navbarRef = useRef<HTMLDivElement>(null);
+    //start of 
+    useEffect(() => {
+        //handleScroll function
+        const handleScroll = () => {
+        if (navbarRef.current) {
+            const navbarTop = navbarRef.current.getBoundingClientRect().top;
 
-        // setIsSticky(navbarTop < 0);
-        // if (initialTop === null) {
-        //     setInitialTop(navbarTop);
-        // }
-
-        if (navbarTop < 0 && !isSticky) {
-            setIsSticky(true);
-        } else if (navbarTop >= 0 && isSticky) {
-            setIsSticky(false);
-            // window.removeEventListener("scroll", handleScroll);
+            if (navbarTop < 0 && !isSticky) {
+                setIsSticky(true);
+            } else if (navbarTop >= 0 && isSticky) {
+                setIsSticky(false);
+            }
         }
-      }
-    };
+        };
 
-    window.addEventListener("scroll", handleScroll);
+        window.addEventListener("scroll", handleScroll);
 
-    // window.addEventListener("scroll", handleScroll);
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, [isSticky]);
+        // window.addEventListener("scroll", handleScroll);
+        return () => {
+        window.removeEventListener("scroll", handleScroll);
+        };
+    }, [isSticky]);//this code only runs when isSticky changes
 
-  return (
-    <div
-      ref={navbarRef}
-      className={`w-full backdrop-grayscale-0 text-white p-4 transition-transform ${
-        isSticky ? "fixed top-0" : "absolute"
-      }`}
-      style={{
-        backgroundColor: "#333", // Darker grey color
-        top: isSticky ? 0 : "75vh", // Start at 75% of the viewport height
-      }}
-    >
-      <nav className="flex justify-around">
-        <a href="#home" className="hover:bg-indigo-500 transition ease-in-out hover:-translate-y-1 hover:translate-y-1 hover:scale-110 rounded-full px-4 py-2 border border-transparent">Home</a>
-      </nav>
-    </div>
-  );
+    return (
+        <div
+        ref={navbarRef}
+        className={`w-4/5 mx-auto backdrop-grayscale-0 
+            text-white p-4 transition-transform rounded-full ${
+            isSticky ? "fixed top-0 left-1/2 transform -translate-x-1/2" : 
+            "absolute left-1/2 transform -translate-x-1/2"
+        }`}
+        style={{
+            backgroundColor: "#333", // Darker grey color
+            top: isSticky ? 0 : "75vh", // Start at 75% of the viewport height
+        }}
+        >
+        <nav className="flex justify-around">
+            <a href="#home" className="hover:bg-indigo-500 transition ease-in-out hover:-translate-y-1 hover:translate-y-1 hover:scale-110 rounded-full px-4 py-2 border border-transparent">Home</a>
+        </nav>
+        </div>
+    );
 };
 
 export default Navbar;
