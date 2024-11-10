94f3d4fe89b64cb82862a2289f8a27fec6232506
PramaYudhistira <pyudhistira02@gmail.com>
2024-11-10 12:15:58 -0600

    satisfied with home section

--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -1,7 +1,5 @@
 import Navbar from "../components/Navbar";
-import WelcomeCard from "@/components/WelcomeCard";
 import EducationCard from "@/components/EducationCard";
-import BackgroundVideo from "@/components/BackgroundVideo";
 import HomeSection from "@/components/HomeSection";
 
 export default function Home() {
@@ -11,7 +9,7 @@ export default function Home() {
       <Navbar />
       <div style={{
         height: "200vh",
-      }}><h1>testing navbar blur</h1></div>
+      }}><h1>hot men kissing</h1></div>
     </div>
   );
   }
--- portfolio/src/components/HomeSection.tsx
+++ portfolio/src/components/HomeSection.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
 
 
 const HomeSection: React.FC = () => {
@@ -17,6 +18,21 @@ const HomeSection: React.FC = () => {
                 <source src="/videos/higher_res.mp4" type="video/mp4" />
                 Your browser does not support the video tag.
             </video>
+            <div className="absolute inset-0 flex flex-col md:items-center justify-center z-10">
+                <div className="self-start text-left max-w-lg pl-4 md:mx-auto pr-5rem">
+                    <h2 className="text-4xl md:text-5xl font-bold mb-4 break-words">Prama</h2>
+                    <h2 className="text-4xl md:text-5xl font-bold mb-4 break-words">Yudhistira</h2>
+                    <p className="text-lg text-left mb-4">Computer Scientist</p>
+                    <div className="flex space-x-4 ">
+                        <a href="https://github.com/PramaYudhistira" target="_blank" rel="noopener noreferrer">
+                            <FaGithub className="text-4xl"/>
+                        </a>
+                        <a href="https://linkedin.com/in/pramayudhistira" target="_blank" rel="noopener noreferrer">
+                            <FaLinkedin className="text-4xl"/>
+                        </a>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
