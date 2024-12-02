36c0eec9881a84a048220698f86f6056d7d26287
PramaYudhistira <pyudhistira02@gmail.com>
2024-12-02 14:20:40 -0600

    added particle background instead

--- portfolio/src/components/HomeSection.tsx
+++ portfolio/src/components/HomeSection.tsx
@@ -1,7 +1,15 @@
 "use client";
-import React, { useEffect, useRef } from "react";
+import React, { useState, useEffect, useMemo } from "react";
 import { FaGithub, FaLinkedin, FaInstagram } from "react-icons/fa";
 import { motion, useInView } from "framer-motion";
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import { loadSlim } from "@tsparticles/slim";
+import {
+    type Container,
+    type ISourceOptions,
+    MoveDirection,
+    OutMode,
+} from "@tsparticles/engine";
 
 
 interface HomeSectionProps {
@@ -10,31 +18,100 @@ interface HomeSectionProps {
 
 
 const HomeSection: React.FC<HomeSectionProps> = ({ nameRef }) => {
-    // const isInView = useInView(nameRef);
-
-    // useEffect(() => {
-    //     if (isInView) {
-    //         console.log("I can see it!");
-    //     } else {
-    //         console.log("I can't see it!");
-    //     }
-    // }, [isInView]);
+    const [init, setInit] = useState(false);
+    useEffect(() => {
+        initParticlesEngine(async (engine) => {
+          await loadSlim(engine);
+        }).then(() => {
+          setInit(true);
+        });
+      }, []);
+      const particlesLoaded = async (container?: Container): Promise<void> => {
+        console.log(container);
+      };
+    
+      const options: ISourceOptions = useMemo(
+        () => ({
+          fullScreen: {
+            enable: false,
+          },
+          background: {
+            color: {
+              value: "#0a0a0a",
+            },
+          },
+          fpsLimit: 120,
+          interactivity: {
+            events: {
+              onClick: {
+                enable: true,
+                mode: "push",
+              },
+              onHover: {
+                enable: true,
+                mode: "repulse",
+              },
+            },
+            modes: {
+              push: {
+                quantity: 4,
+              },
+              repulse: {
+                distance: 200,
+                duration: 0.4,
+              },
+            },
+          },
+          particles: {
+            color: {
+              value: "#ffffff",
+            },
+            links: {
+              color: "#ffffff",
+              distance: 150,
+              enable: true,
+              opacity: 0.5,
+              width: 1,
+            },
+            move: {
+              direction: MoveDirection.none,
+              enable: true,
+              outModes: {
+                default: OutMode.out,
+              },
+              random: false,
+              speed: 3,
+              straight: false,
+            },
+            number: {
+              density: {
+                enable: true,
+              },
+              value: 120,
+            },
+            opacity: {
+              value: 0.5,
+            },
+            shape: {
+              type: "circle",
+            },
+            size: {
+              value: { min: 1, max: 5 },
+            },
+          },
+          detectRetina: true,
+        }),
+        [],
+      );
 
     return (
         <div className="relative h-[70vh] overflow-hidden relative z-0">
-            <video
-            autoPlay
-            muted
-            loop
-            playsInline
-            className="w-full h-full object-cover"
-            style={{
-            objectFit: 'cover'
-            }}
-            >
-                <source src="/videos/higher_res.mp4" type="video/mp4" />
-                Your browser does not support the video tag.
-            </video>
+            <Particles 
+            id="tsparticles"
+            particlesLoaded={particlesLoaded}
+            options={options}
+            />
+
             <div className="absolute inset-0 flex flex-col md:items-center justify-center z-10">
                 <div className="self-start text-left max-w-lg pl-4 md:mx-auto pr-5rem">
                     <h2 className="text-4xl md:text-5xl lg:text-8xl font-bold mb-4 break-words">Prama</h2>
