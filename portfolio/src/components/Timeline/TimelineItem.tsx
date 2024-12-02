f424975db3fabb828f2adeada18e975773616983
PramaYudhistira <pyudhistira02@gmail.com>
2024-12-02 01:27:34 -0600

    timeline done, this project will finally get over with

--- portfolio/src/components/Timeline/TimelineItem.tsx
+++ portfolio/src/components/Timeline/TimelineItem.tsx
@@ -23,23 +23,35 @@ const TimelineItem: React.FC<TimelineItemProps> = ({
     const isInView = useInView(ref);
 
     return (
-        <motion.div
-        ref={ref}
-        initial={{ opacity: 0}}//why is y: 50 
-        animate={{ opacity: isInView ? 1 : 0}}
-        transition={{ duration: 0.5, delay: index * 0.2 }}
-        className="p-4 bg-gray-900 rounded-lg shadow-lg max-w-md z-10"
-        >
-            <div className="flex items-center">
-                <div className="w-12  h-12 bg-white rounded-full flex items-center justify-center overflow-hidden">
-                    <img src={logoSrc} alt={`${company} logo`} className="w-8 h-8 transform scale-125" />
+        <div className="relative">
+            {index % 2 === 0 && (
+                <motion.div className="hidden sm:block absolute right-full top-1/2 transform -translate-y-1/2 w-14 h-0.5 bg-gray-300 z-0 opacity-30"
+                initial={{ opacity: 0 }}
+                animate={{ opacity: isInView ? 0.3: 0 }}></motion.div>
+            )}
+            {index % 2 !== 0 && (
+                <motion.div className="hidden sm:block absolute left-full top-1/2 transform -translate-y-1/2 w-14 h-0.5 bg-gray-300 z-0 opacity-30"
+                initial={{ opacity: 0 }}
+                animate={{ opacity: isInView ? 0.3: 0 }}></motion.div>
+            )}
+            <motion.div
+                ref={ref}
+                initial={{ opacity: 0}}
+                animate={{ opacity: isInView ? 1 : 0}}
+                transition={{ duration: 0.5, delay: index * 0.2 }}
+                className="p-4 bg-gray-900 rounded-lg shadow-lg max-w-md z-10"
+            >
+                <div className="flex items-center">
+                    <div className="w-12 h-12 bg-white rounded-full flex items-center justify-center overflow-hidden">
+                        <img src={logoSrc} alt={`${company} logo`} className="w-8 h-8 transform scale-125" />
+                    </div>
+                    <h3 className="text-xl font-bold text-white ml-3">{company}</h3> 
                 </div>
-                <h3 className="text-xl font-bold text-white ml-3">{company}</h3> 
-            </div>
-            <h4 className="text-lg text-gray-400">{title}</h4>
-            <span className="text-gray-500">{dateRange}</span>
-            <p className="text-gray-300">{description}</p>
-        </motion.div>
+                <h4 className="text-lg text-gray-400">{title}</h4>
+                <span className="text-gray-500">{dateRange}</span>
+                <p className="text-gray-300">{description}</p>
+            </motion.div>
+        </div>
     );
 };
 
