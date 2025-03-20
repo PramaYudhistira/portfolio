a84870658e89d5df8aea8d680c2bed0868d9411e
PramaYudhistira <pyudhistira02@gmail.com>
2025-03-20 18:53:07 -0400

    removed pussyass particle.js it looks so shit

---
+++ portfolio/src/@types/vanta.d.ts
@@ -0,0 +1,34 @@
+declare module "vanta/dist/vanta.net.min" {
+    const NET: any;
+    export default NET;
+}
+
+declare module "vanta/dist/vanta.waves.min" {
+    const WAVES: any;
+    export default WAVES;
+}
+
+declare module "vanta/dist/vanta.fog.min" {
+    const FOG: any;
+    export default FOG;
+}
+
+declare module "vanta/dist/vanta.clouds.min" {
+    const CLOUDS: any;
+    export default CLOUDS;
+}
+
+declare module "vanta/dist/vanta.birds.min" {
+    const BIRDS: any;
+    export default BIRDS;
+}
+
+declare module "vanta/dist/vanta.globe.min" {
+    const GLOBE: any;
+    export default GLOBE;
+}
+
+declare module "vanta/dist/vanta.rings.min" {
+    const RINGS: any;
+    export default RINGS;
+}
--- portfolio/src/components/HomeSection.tsx
+++ portfolio/src/components/HomeSection.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState, useEffect, useMemo } from "react";
+import React, { useState, useEffect, useRef, useMemo } from "react";
 import { FaGithub, FaLinkedin, FaInstagram } from "react-icons/fa";
 import { motion, useInView } from "framer-motion";
 import Particles, { initParticlesEngine } from "@tsparticles/react";
@@ -10,7 +10,9 @@ import {
     MoveDirection,
     OutMode,
 } from "@tsparticles/engine";
-
+import * as THREE from "three";
+import GLOBE from "vanta/dist/vanta.globe.min";
+import RINGS from "vanta/dist/vanta.rings.min";
 
 interface HomeSectionProps {
     nameRef: React.RefObject<HTMLHeadingElement>;
@@ -18,122 +20,80 @@ interface HomeSectionProps {
 
 
 const HomeSection: React.FC<HomeSectionProps> = ({ nameRef }) => {
-    const [init, setInit] = useState(false);
-    useEffect(() => {
-        initParticlesEngine(async (engine) => {
-          await loadSlim(engine);
-        }).then(() => {
-          setInit(true);
-        });
-      }, []);
-      const particlesLoaded = async (container?: Container): Promise<void> => {
-        console.log(container);
-      };
-    
-      const options: ISourceOptions = useMemo(
-        () => ({
-          fullScreen: {
-            enable: false,
-            zIndex: 0,
-          },
-          background: {
-            color: {
-              value: "#0a0a0a",
-            },
-          },
-          fpsLimit: 120,
-          interactivity: {
-            events: {
-            //   onClick: {
-            //     enable: true,
-            //     mode: "push",
-            //   },
-              onHover: {
-                enable: true,
-                mode: "repulse",
-              },
-            },
-            modes: {
-              push: {
-                quantity: 4,
-              },
-              repulse: {
-                distance: 200,
-                duration: 0.4,
-              },
-            },
-          },
-          particles: {
-            color: {
-              value: "#ffffff",
-            },
-            links: {
-              color: "#ffffff",
-              distance: 150,
-              enable: true,
-              opacity: 0.5,
-              width: 1,
-            },
-            move: {
-              direction: MoveDirection.none,
-              enable: true,
-              outModes: {
-                default: OutMode.out,
-              },
-              random: false,
-              speed: 5,
-              straight: false,
-            },
-            number: {
-              density: {
-                enable: true,
-              },
-              value: 120,
-            },
-            opacity: {
-              value: 0.5,
-            },
-            shape: {
-              type: "circle",
-            },
-            size: {
-              value: { min: 1, max: 5 },
-            },
-          },
-          detectRetina: true,
-        }),
-        [],
+  const [vantaEffect, setVantaEffect] = useState<any>(null);
+  const vantaRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (!vantaEffect && typeof window !== "undefined") {
+      setVantaEffect(
+        GLOBE({
+          el: vantaRef.current,
+          THREE: THREE,
+          mouseControls: true,
+          touchControls: false,
+          gyroControls: false,
+          minHeight: 200.00,
+          minWidth: 200.00,
+          scale: 1.00,
+          scaleMobile: 1.00,
+          backgroundColor:0x0,
+          color: 0x6384b6,
+          color2: 0xbd20e3,
+        })
       );
+      // setVantaEffect(
+      //   RINGS({
+      //     el: vantaRef.current,
+      //     THREE:THREE, 
+      //     mouseControls: true,
+      //     touchControls: true,
+      //     gyroControls: false,
+      //     minHeight: 200.00,
+      //     minWidth: 200.00,
+      //     scale: 1.00,
+      //     scaleMobile: 1.00,
+      //     color: 0x9cb67d,
+      //     backgroundColor: 0x202428,
+      //   })
+      // );
+    }
 
-    return (
-        <div className="relative h-[70vh] overflow-hidden relative z-0">
-            <Particles 
-            id="tsparticles"
-            particlesLoaded={particlesLoaded}
-            options={options}
-            className="absolute inset-0 h-full w-full"
-            />
+    return () => {
+      if (vantaEffect) vantaEffect.destroy();
+    };
+  }, [vantaEffect]);
 
-            <div className="absolute inset-0 flex flex-col md:items-center justify-center z-10">
-                <div className="self-start text-left max-w-lg pl-4 md:mx-auto pr-5rem">
-                    <h2 className="text-4xl md:text-5xl lg:text-8xl font-bold mb-4 break-words">Prama</h2>
-                    <motion.h2 ref={nameRef} className="text-4xl md:text-5xl lg:text-8xl font-bold mb-4 break-words">Yudhistira</motion.h2>
-                    <p className="text-2xl lg:text-4xl text-left mb-4">Computer Scientist</p>
-                    <div className="flex space-x-4 ">
-                        <a href="https://github.com/PramaYudhistira" target="_blank" rel="noopener noreferrer">
-                            <FaGithub className="text-3xl lg:text-5xl"/>
-                        </a>
-                        <a href="https://linkedin.com/in/pramayudhistira" target="_blank" rel="noopener noreferrer">
-                            <FaLinkedin className="text-3xl lg:text-5xl"/>
-                        </a>
-                        {/* <a href="https://linkedin.com/in/pramayudhistira" target="_blank" rel="noopener noreferrer">
-                            <FaInstagram className="text-3xl lg:text-5xl"/>
-                        </a> */}
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="relative h-[70vh] overflow-hidden relative z-0">
+
+      <div ref={vantaRef} className="absolute inset-0 h-full w-full" />
+      {/* <Particles 
+      id="tsparticles"
+      particlesLoaded={particlesLoaded}
+      options={options}
+      className="absolute inset-0 h-full w-full"
+      /> */}
+
+      <div className="absolute inset-0 flex flex-col md:items-center justify-center z-10">
+        <div className="self-start text-left max-w-lg pl-4 md:mx-auto pr-5rem">
+          <h2 className="text-4xl md:text-5xl lg:text-8xl font-bold mb-4 break-words">Prama</h2>
+          <motion.h2 ref={nameRef} className="text-4xl md:text-5xl lg:text-8xl font-bold mb-4 break-words">Yudhistira</motion.h2>
+          <p className="text-2xl lg:text-4xl text-left mb-4">Computer Scientist</p>
+          <div className="flex space-x-4 ">
+            <a href="https://github.com/PramaYudhistira" target="_blank" rel="noopener noreferrer">
+              <FaGithub className="text-3xl lg:text-5xl"/>
+            </a>
+            <a href="https://linkedin.com/in/pramayudhistira" target="_blank" rel="noopener noreferrer">
+              <FaLinkedin className="text-3xl lg:text-5xl"/>
+            </a>
+            {/* <a href="https://linkedin.com/in/pramayudhistira" target="_blank" rel="noopener noreferrer">
+              <FaInstagram className="text-3xl lg:text-5xl"/>
+            </a> */}
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 
